164fe32a37a3b3fdc9dd4c809033971e877a3086
zzzgit <zzzgoo@gmail.com>
2026-01-19 06:59:39 +0800

    222

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,26 @@ import { defineConfig } from 'vite'
 import babel from 'vite-plugin-babel'
 
 export default defineConfig({
-  plugins: [
-    babel({
-      babelConfig: {
-        presets: [
-          [
-            '@babel/preset-react',
-            {
-              runtime: 'automatic',
-              importSource: '.'
-            }
-          ]
-        ]
-      }
-    })
-  ],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
+	plugins: [
+		babel({
+			babelConfig: {
+				presets: [
+					[
+						'@babel/preset-react',
+						{
+							runtime: 'automatic',
+							importSource: '.',
+						},
+					],
+				],
+			},
+		}),
+	],
+	build: {
+		outDir: 'dist',
+	},
+	server: {
+		port: 3000,
+		open: true,
+	},
 })
