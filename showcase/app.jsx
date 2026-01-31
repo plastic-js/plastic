0a2eca6fc6589b9a051c1f4c4bb1d181b2fb3d15
zzzgit <zzzgoo@gmail.com>
2026-01-31 17:26:09 +0800

    222

--- showcase/app.jsx
+++ showcase/app.jsx
@@ -1,6 +1,6 @@
 import { computed, runMount, signal } from '../src/jsx-runtime.js'
-import Label from '../showcase/components/Label.jsx'
-import If from '../src/If.jsx'
+// import Label from '../showcase/components/Label.jsx'
+// import If from '../src/If.jsx'
 import './global.css'
 
 // 创建响应式状态
@@ -17,50 +17,7 @@ const App = ()=> {
 	}
 	return (
 		<div className='container'>
-			{/* // 永遠不會切換，沒關係。就用這種效果，因為我們不反對tenary這種語法 */}
-			{ showLabel && <Label text='This is a label component--foo' /> }
-			<div />
-			<If when={showLabel}>
-				<Label text='This is a label component--bar' />
-			</If>
-			<div className='counter'>
-				<p>
-					Count:
-					{ count}
-				</p>
-				<p>
-					Double Count:
-					{ doubleCount }
-				</p>
-			</div>
-			<div className='buttons'>
-				<button
-					onClick={()=> count(count() + 1)}
-				>
-					Increment
-				</button>
-				<button
-					onClick={()=> count(count() - 1)}
-				>
-					Decrement
-				</button>
-				<button
-					onClick={()=> count(0)}
-				>
-					Reset
-				</button>
-				<button
-					onClick={handleClick}
-				>
-					Toggle Label
-				</button>
-			</div>
-			<div className='info'>
-				<p>
-					Status:
-					{level}
-				</p>
-			</div>
+			<span>2222</span>
 		</div>
 
 	)
@@ -69,4 +26,4 @@ const App = ()=> {
 const appElement = App()
 document.body.appendChild(appElement)
 // 触发 mount 事件以启动响应式效果
-runMount(appElement)
+// runMount(appElement)
